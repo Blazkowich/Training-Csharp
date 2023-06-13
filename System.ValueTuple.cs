bd16e163b92535fa962dbee7e1d8a67a0ae5bbb7
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-06-13 13:11:13 +0400

    Update System.ValueTuple.cs

--- System.ValueTuple.cs
+++ System.ValueTuple.cs
@@ -16,5 +16,17 @@ class Program
         Console.WriteLine(messi.playerNo);
         Console.WriteLine(messi.playerName);
         Console.WriteLine(messi.playerGoals);
+        Console.WriteLine();
+
+        var ronaldoGoals = (Year2017: 64, Year2016: 60, Year2015: 50);
+
+        var ronaldhinio = (playerNo: 7, playerName: "Ronaldo", playerGoals: ronaldoGoals);
+
+        Console.WriteLine(ronaldhinio.playerNo);
+        Console.WriteLine(ronaldhinio.playerName);
+        Console.WriteLine("2015 " + ronaldhinio.playerGoals.Year2015);
+        Console.WriteLine("2016 " + ronaldhinio.playerGoals.Year2016);
+        Console.WriteLine("2017 " + ronaldhinio.playerGoals.Year2017);
+        Console.WriteLine();
     }
 }
